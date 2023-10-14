a3c645002e41634796dbb6d0653fe9fddd93d494
xeanyu <xeanyu@163.com>
2023-10-14 11:03:19 +0800

    init

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -5,49 +5,47 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct ColorClassicStruct {
-    name: String,
-    hex:  String,
+    // TODO: Something goes here
+    name:String,
+    hex:String
 }
 
-struct ColorTupleStruct(String, String);
+struct ColorTupleStruct(String,String/* TODO: Something goes here */);
 
 #[derive(Debug)]
-struct UnitLikeStruct;
+struct UnitStruct;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        let green = ColorClassicStruct { name: "green".into(),
-                                         hex:  "#00FF00".into() };
-
-        assert_eq!(green.red, 0);
-        assert_eq!(green.green, 255);
-        assert_eq!(green.blue, 0);
+        let green = ColorClassicStruct{
+            name : String::from("green"),
+            hex : String::from("#00FF00"),
+        };
+        assert_eq!(green.name, "green");
+        assert_eq!(green.hex, "#00FF00");
     }
 
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        let green = ColorTupleStruct("green".into(), "#00FF00".into());
+        let green = ColorTupleStruct(String::from("green"),String::from("#00FF00"));
 
-        assert_eq!(green.0, 0);
-        assert_eq!(green.1, 255);
-        assert_eq!(green.2, 0);
+        assert_eq!(green.0, "green");
+        assert_eq!(green.1, "#00FF00");
     }
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        // TODO: Instantiate a unit struct!
         let unit_struct = UnitStruct;
         let message = format!("{:?}s are fun!", unit_struct);
-        assert_eq!(message, "UnitLikeStructs are fun!");
+
+        assert_eq!(message, "UnitStructs are fun!");
     }
 }
