8e34b1884482a0c53757f2dadb54830268890630
xeanyu <xeanyu@163.com>
2023-10-18 01:52:18 +0800

    init

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -5,23 +5,23 @@
 // the previous one work.
 //
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
-// hint.fn main() {}fn main() {}
+// hint.
+
+
+fn main() {
+
+}
+
 
 #[cfg(test)]
 mod tests {
-    extern "C" {
-        fn my_demo_function(a: u32);
-        fn my_demo_function_alias(a: u32);
-    }
+    use super::*;
 
     #[test]
     fn test_success() {
-        unsafe {
-            my_demo_function(123);
-        }
-        
-        unsafe {
-            my_demo_function_alias(456);
-        }
+        #[cfg(feature = "pass")]
+        return;
+
+        panic!("no cfg set");
     }
 }
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,15 +27,19 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-mod foo {
-    pub fn my_demo_function(a: u32) -> u32 {
-        a
-    }
-}
 
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
     fn my_demo_function_alias(a: u32) -> u32;
+}
+
+mod foo {
+    // No `extern` equals `extern "Rust"`.
+
+    #[no_mangle]
+    fn my_demo_function(a: u32) -> u32 {
+        a
+    }
 }
 
 #[cfg(test)]
@@ -44,12 +48,15 @@ mod tests {
 
     #[test]
     fn test_success() {
-        // SAFETY: We know those functions are aliases of a safe Rust function.
+        // The externally imported functions are UNSAFE by default
+        // because of untrusted source of other languages. You may
+        // wrap them in safe Rust APIs to ease the burden of callers.
+        //
+        // SAFETY: We know those functions are aliases of a safe
+        // Rust function.
         unsafe {
-            let result1 = my_demo_function(123);
-            let result2 = my_demo_function_alias(456);
-            assert_eq!(result1, 123);
-            assert_eq!(result2, 456);
+            my_demo_function(123);
+            my_demo_function_alias(456);
         }
     }
 }
